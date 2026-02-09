d901afb7feca9861e32c6ea625433851190b58f0
Santu Saha <tuhinsaha2362@gmail.com>
2026-02-09 19:21:08 +0530

    add panel root page redirect

---
+++ app/panel/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function PanelPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/panel/dashboard");
+  }, [router]);
+
+  return null;
+}
